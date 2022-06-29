36ac635559aae1148b46c5f0e09bc6355e0cca88
nicholasneo78 <nicholasneo78@gmail.com>
2022-06-29 10:21:49 +0800

    code update

--- tasks/audio/preprocessing/reduce_noise.py
+++ tasks/audio/preprocessing/reduce_noise.py
@@ -8,18 +8,30 @@ class ReduceNoise:
             input_audio_path: audio path that is to be preprocessed (to be noise reduced)
             output_audio_path: audio path of the noise reduced audio
         '''
-        self.input_audio_path =input_audio_path
+        self.input_audio_path = input_audio_path
         self.output_audio_path = output_audio_path
 
     def reduce_noise(self) -> None:
-        
+        '''
+            main method to reduce noise of the audio
+        '''
 
+        # load data
+        rate, data = wavfile.read(self.input_audio_path)
 
-# load data
-rate, data = wavfile.read("test.wav")
+        data = np.mean(data, axis=1, dtype=data.dtype)
 
-data = np.mean(data, axis=1, dtype=data.dtype)
+        # perform noise reduction
+        reduced_noise = nr.reduce_noise(y=data, sr=rate)
 
-# perform noise reduction
-reduced_noise = nr.reduce_noise(y=data, sr=rate)
-wavfile.write("test_reduce.wav", rate, reduced_noise)
+        # export the noise reduced audio file
+        wavfile.write(self.output_audio_path, rate, reduced_noise)
+
+    def __call__(self):
+        return self.reduce_noise()
+
+if __name__ == '__main__':
+    reduce_noise_audio = ReduceNoise(input_audio_path='/preproc/batched_1_hr_audio/CH10_batch_0.wav', 
+                                     output_audio_path='/preproc/batched_1_hr_audio/CH10_batch_0_reduce_noise.wav')
+
+    reduce_noise_audio()
