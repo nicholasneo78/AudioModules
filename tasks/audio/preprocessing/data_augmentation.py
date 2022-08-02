f8165a1756857ba04f998bda088cc16e7f2b0fbe
nicholasneo78 <nicholasneo78@gmail.com>
2022-08-02 11:51:10 +0800

    added new config

--- tasks/audio/preprocessing/data_augmentation.py
+++ tasks/audio/preprocessing/data_augmentation.py
@@ -1,45 +1,14 @@
-import random
 import librosa
-import numpy as np
 import soundfile as sf
+from audiomentations import Compose, AddGaussianNoise, PitchShift, HighPassFilter, LowPassFilter, BandPassFilter
 
-# Python 3.8
-# install matplotlib, librosa
-# install python3-tk -> sudo apt install python3-tk
-
-
-def add_white_noise(signal, noise_percentage_factor):
-    noise = np.random.normal(0, signal.std(), signal.size)
-    augmented_signal = signal + noise * noise_percentage_factor
-    return augmented_signal
-
-
-def time_stretch(signal, time_stretch_rate):
-    """Time stretching implemented with librosa:
-    https://librosa.org/doc/main/generated/librosa.effects.pitch_shift.html?highlight=pitch%20shift#librosa.effects.pitch_shift
-    """
-    return librosa.effects.time_stretch(signal, time_stretch_rate)
-
-
-def pitch_scale(signal, sr, num_semitones):
-    """Pitch scaling implemented with librosa:
-    https://librosa.org/doc/main/generated/librosa.effects.pitch_shift.html?highlight=pitch%20shift#librosa.effects.pitch_shift
-    """
-    return librosa.effects.pitch_shift(signal, sr, num_semitones)
-
-
-def random_gain(signal, min_factor=0.1, max_factor=0.12):
-    gain_rate = random.uniform(min_factor, max_factor)
-    augmented_signal = signal * gain_rate
-    return augmented_signal
-
-
-def invert_polarity(signal):
-    return signal * -1
-
+augment = Compose([
+    #AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.001, p=1),
+    BandPassFilter(min_center_freq=20.0, max_center_freq=3000.0, p=1)
+])
 
 if __name__ == "__main__":
-    signal, sr = librosa.load("/preproc/datasets/example_libri/input_1.wav")
-    augmented_signal = add_white_noise(signal, 0.03)
-    #augmented_signal = random_gain(signal)
-    sf.write("/preproc/datasets/example_libri/output_1w.wav", augmented_signal, sr)
+    signal, sr = librosa.load("/preproc/datasets/example_libri/input_1.wav", sr=16000)
+    print(sr)
+    augmented_signal = augment(signal, sr)
+    sf.write("/preproc/datasets/example_libri/output_1d.wav", augmented_signal, sr)
